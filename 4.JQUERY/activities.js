543c4a58391640b9e92538228ce598cd967921e7
FDC-Junery <fdc.caballes@gmail.com>
2020-02-11 14:59:32 +0800

    added expand and colapse

--- 4.JQUERY/activities.js
+++ 4.JQUERY/activities.js
@@ -42,23 +42,71 @@ function calcAverage() {
 	var filipino_subj = parseInt(document.getElementById('filipino_subj').value);
 
 	var average = (pe_subj + math_subj + science_subj + english_subj + filipino_subj) / 5;
+	var averageletter = "";
 
-	document.getElementById('displayAverage').style.display = "block";
-	document.getElementById('displayAverage').innerHTML = "Average: " + average;
+	switch(true) {
+    	case average < 60: 
+        	averageletter = "F";
+            break;
+    	case (average > 60 && average < 70): 
+    		averageletter = "D";
+            break;
+    	case (average > 70 && average < 80): 
+    		averageletter = "C";
+            break;
+    	case (average > 80 && average < 90): 
+    		averageletter = "B";
+            break;
+    	case (average > 90 && average < 100): 
+    		averageletter = "A";
+            break;
+    	default :
+        	alert("Average is not on range. Bright ka ghorl?");
+        	break;
+    }
+
+    document.getElementById('displayAverage').style.display = "block";
+	document.getElementById('displayAverage').innerHTML = "Average: " + average + "<br> Grade: " + averageletter;
 }
 
 function displayBeepBoop() {
 	var msg = "";
-	for(var i = 0; i<=100; i++) {
-    	if((i%3) == 0) {
-        	msg += "beep";
-        }
-        if ((i%5) == 0) {
-        	msg += "boop";
-        }
-        document.getElementById('displaybeepboop').innerHTML += i + msg + "<br>";
-        msg = "";
-    }
+	if(beepbeepButtonCheck("displaybeepboopbtn", "Display Beep Boop (0 - 100)")) {
+		document.getElementById('displaybeepboop').style.display = "block";
+		document.getElementById('displaybeepboop').innerHTML = "";
+		for(var i = 0; i<=100; i++) {
+	    	if((i%3) == 0) {
+	        	msg += " beep";
+	        }
+	        if ((i%5) == 0) {
+	        	msg += " boop";
+	        }
+	        document.getElementById('displaybeepboop').innerHTML += i + msg + "<br>";
+	        msg = "";
+	    }			
+		changeButtonText("displaybeepboopbtn", "Hide");
+	} else {
+		changeButtonText("displaybeepboopbtn", "Display Beep Boop (0 - 100)");
+		document.getElementById('displaybeepboop').style.display = "none";
+	}
+	
+}
+
+function changeButtonText(id, value) {
+	console.log(id);
+	console.log(value);
+	document.getElementById(id).innerHTML = value;
+}
+
+function beepbeepButtonCheck(id, value) {
+	var text = document.getElementById(id).innerHTML;
+	var retBoolean = true;
+	console.log("Text: " + text);
+
+	if(text == value) {
+		return true;
+	} 
+	return false;
 }
 
 displayTriangle();
